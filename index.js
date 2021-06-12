77fc52816dd813a433e9d2c0aa64ac12562818b5
Marwa  Awwad <marwaawwad10@gmail.com>
2021-06-12 23:03:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -116,7 +116,46 @@ const fi = (function() {
 
          return fn.sort()
        },
-
+       flatten: function(collection, shallow, newArr=[]) {
+             if (!Array.isArray(collection)) return newArr.push(collection)
+             if (shallow) {
+               for (let val of collection)
+                 Array.isArray(val) ? this.unpack(newArr, val) : newArr.push(val)
+             } else {
+               for (let val of collection) {
+                 this.flatten(val, false, newArr)
+               }
+             }
+             return newArr
+           },
+       
+           uniqSorted: function(collection, iteratee) {
+             const sorted = [collection[0]]
+             for (let idx = 1; idx < collection.length; idx++) {
+               if (sorted[idx-1] !== collection[idx])
+                 sorted.push(collection[idx])
+             }
+             return sorted
+           },
+       
+           uniq: function(collection, sorted=false, iteratee=false) {
+             if (sorted) {
+               return fi.uniqSorted(collection, iteratee)
+             } else if (!iteratee) {
+               return Array.from(new Set(collection))
+             } else {
+               const modifiedVals = new Set()
+               const uniqVals = new Set()
+               for (let val of collection) {
+                 const moddedVal = iteratee(val)
+                 if (!modifiedVals.has(moddedVal)) {
+                   modifiedVals.add(moddedVal)
+                   uniqVals.add(val)
+                 }
+               }
+               return Array.from(uniqVals)
+             }
+           },
 
   }
 })()
